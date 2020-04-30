52387d18ae0dd193d5f42f14ae6f9cd1f958f500
Arjo <arjo129@gmail.com>
2020-04-30 18:14:09 +0800

    got qr code to be generated :)

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@ use std::net::SocketAddr;
 
 #[derive(Debug, Copy, Clone)]
 enum IfaceError {
-    FailedToReadProcFs
+    FailedToReadProcFs,
+    UnableToGetIpForWiFi,
+    NoNetworkIface
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -52,14 +54,32 @@ fn get_wireless_interface()->Result<Vec<String>, IfaceError> {
 }
 
 fn get_ip_address_given_iface_name(iface_name: String) -> Option<SocketAddr>{
-    for iface in
-    ifaces::Interface::get_all().unwrap()
-        .into_iter() {
-            if iface.name == iface_name {
-                return iface.addr;
-            }
+    let mut res: Option<SocketAddr> = None;
+    for iface in ifaces::Interface::get_all().unwrap().into_iter() {
+        if iface.name == iface_name {
+            if let Some(SocketAddr::V4(ipaddr)) = iface.addr {res = iface.addr;}
+        }
     }
-    None
+    res
+}
+
+fn get_ip() ->  Result<String, IfaceError> {
+    let wifi_interfaces = get_wireless_interface();
+    match wifi_interfaces {
+        Ok(interfaces) => {
+            for i in interfaces {
+                let address = get_ip_address_given_iface_name(i);
+                match address {
+                    Some(addr) => return Ok(addr.ip().to_string()),
+                    None => return Err(IfaceError::UnableToGetIpForWiFi)
+                }
+            }
+            return Err(IfaceError::NoNetworkIface);
+        },
+        Err(e) => {
+            return Err(e);
+        }
+    }
 }
 
 fn main() {
@@ -70,10 +90,16 @@ fn main() {
 
     application.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
-        println!("{:?}", get_wireless_interface());
-        window.set_title("First GTK+ Program");
-        window.set_default_size(350, 70);
-        let code = QrCode::new(b"http://www.google.com").unwrap();
+        window.set_title("Remote Tablet Program");
+        window.set_default_size(350, 350);
+        let ip = get_ip();
+        match ip.clone() {
+            Ok(_) =>{}
+            Err(e) => {println!("Error fetching IP {:?}", e); return;}
+        }
+        let ip = format!("http://{}:8007", ip.unwrap());
+        println!("{}", ip);
+        let code = QrCode::new(ip).unwrap();
         let image = DynamicImage::ImageRgb8(code.render::<Rgb<u8>>().build());
         let mut buff = Vec::<u8>::new(); 
         let res = image.write_to(&mut buff, ImageOutputFormat::Png);
@@ -86,6 +112,7 @@ fn main() {
                     let image_view = Image::new_from_pixbuf(pixbuf.as_ref());
                     window.add(&image_view);
                     window.show_all();
+                    println!("Generated qr code");
                 });
                 loader.close();
             },
